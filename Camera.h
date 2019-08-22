e37140b4d587864dbbc757e6347098ede9039639
AnaYaeR <42159669+AnaYaeR@users.noreply.github.com>
2019-08-22 19:46:59 -0400

    Added CameraProperty Type and implemented Get()

--- Camera.h
+++ Camera.h
@@ -6,12 +6,24 @@
 class Camera
 {
 public:
+
+	typedef enum
+	{
+		CAMERA_PERSPECTIVE,
+		CAMERA_WHERE,
+		CAMERA_FORWARD,
+		CAMERA_FOV,
+		CAMERA_UP
+	} CameraProperty;
+
+
 	Camera(const glm::vec3& pos, float fov, float aspect, float zNear, float zFar)
 	{
 		Perspective = glm::perspective(fov, aspect, zNear, zFar);
 		Where = pos;
 		Forward = glm::vec3(0, 0, 1);
 		Up = glm::vec3(0, 1, 0);
+		this->fov = fov;
 	}
 	
 	inline glm::mat4 GetViewProjection() const
@@ -19,9 +31,33 @@ public:
 		return Perspective * glm::lookAt(Where, Where + Forward, Up);
 	}
 
+	inline void* Get(CameraProperty prop)
+	{
+		switch (prop)
+		{
+		case CAMERA_WHERE:
+			return &Where;
+			break;
+		case CAMERA_FORWARD:
+			return &Forward;
+			break;
+		case CAMERA_UP:
+			return &Up;
+			break;
+		case CAMERA_PERSPECTIVE:
+			return &Perspective;
+			break;
+		case CAMERA_FOV:
+			return &fov;
+			break;
+		default: return NULL; break;
+		}
+	}
+
 private:
 	glm::mat4 Perspective;
 	glm::vec3 Where;
 	glm::vec3 Forward;
 	glm::vec3 Up;
+	float fov;
 };
